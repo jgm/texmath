17a26cd523cbadc7649061a679c0469e2d0488ed
John MacFarlane <jgm@berkeley.edu>
2009-07-01 11:20:06 -0700

    Use xhtml for output of test program.

--- testTeXMathML.hs
+++ testTeXMathML.hs
@@ -3,7 +3,17 @@ module Main where
 import Text.TeXMath
 import Text.XML.Light
 
+inHtml :: Element -> Element
+inHtml x =
+  add_attr (Attr (unqual "xmlns") "http://www.w3.org/1999/xhtml") $
+  unode "html" $
+    [ unode "head" $
+        add_attr (Attr (unqual "content") "application/xhtml+xml; charset=UTF-8") $
+        add_attr (Attr (unqual "http-equiv") "Content-Type") $
+        unode "meta" ()
+    , unode "body" x ]
+
 main :: IO ()
 main = do
   inp <- getContents
-  putStr . ppTopElement . texMathToMathML DisplayBlock $! inp
+  putStr . ppTopElement . inHtml . texMathToMathML DisplayBlock $! inp
