aec977cb28c6f41f3700927bf83afe0a24572020
John MacFarlane <jgm@berkeley.edu>
2009-07-05 11:59:29 -0700

    Use JSON in CGI script.

--- cgi/texmath-cgi.hs
+++ cgi/texmath-cgi.hs
@@ -2,17 +2,18 @@ module Main where
 import Network.CGI
 import Text.XML.Light
 import Text.TeXMath
-import Data.Maybe (fromJust)
+import Data.Maybe (fromMaybe)
 import Control.Monad
+import Text.JSON
 
 cgiMain :: CGI CGIResult
 cgiMain = do
-  latexFormula <- liftM fromJust $ getInput "latexFormula"
+  latexFormula <- liftM (fromMaybe "") $ getInput "latexFormula"
   setHeader "Content-type" "text/xhtml; charset=UTF-8"
-  output . ppElement $
+  output . encodeStrict $
     case texMathToMathML DisplayBlock latexFormula of
-       Left e     -> unode "p" e
-       Right v    -> v 
+       Left e     -> toJSObject [("success", JSBool False), ("error", JSString $ toJSString e)]
+       Right v    -> toJSObject [("success", JSBool True), ("mathml", JSString $ toJSString $ ppElement v)]
 
 main :: IO ()
 main = runCGI $ handleErrors cgiMain
