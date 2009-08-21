5ca8eac75df7f398ffcd709bdbd153949e6dd1b2
John MacFarlane <jgm@berkeley.edu>
2009-08-20 18:17:05 -0700

    Improved appearance of test output.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -4,9 +4,9 @@ for t in *.tex; do
     $TESTPROG <$t >tmp
     diff ${t%.tex}.xhtml tmp >tmpdiff
     if [ "$?" -ne "0" ]; then
-        echo "Test $t failed (< expected, > actual):"
+        echo "Test ${t%.tex} FAILED (< expected, > actual):"
         cat tmpdiff
     else
-        echo "Test $t passed."
+        echo "Test ${t%.tex} PASSED"
     fi
 done
