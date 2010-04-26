c7ab39b3627a6cd9a99e0d4df062be93d2823d32
John MacFarlane <jgm@berkeley.edu>
2010-04-25 20:43:59 -0700

    Added some comments to runtests.sh.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Note: this should be run from within the tests directory
+# Make sure you've set the 'test' flag using Cabal:
+# cabal install -ftest
+# Otherwise the test program won't be built.
 TESTPROG=../dist/build/testTeXMathML/testTeXMathML
 for t in *.tex; do
     $TESTPROG <$t >tmp
