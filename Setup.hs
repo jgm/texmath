5ccb17bf6dd93afd25306ed0407d7dc82e8ff363
John MacFarlane <jgm@berkeley.edu>
2010-04-25 22:38:23 -0700

    Added cabal test hook, so tests can be run with 'cabal test'.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,7 @@
 import Distribution.Simple
-main = defaultMain
+import System.Process (system)
+import System.Exit
+
+main = defaultMainWithHooks $ simpleUserHooks { runTests  = runTestSuite }
+
+runTestSuite _ _ _ _ = system "cd tests && ./runtests.sh" >>= exitWith
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -3,14 +3,18 @@
 # Make sure you've set the 'test' flag using Cabal:
 # cabal install -ftest
 # Otherwise the test program won't be built.
+# Exit status is number of failed tests.
 TESTPROG=../dist/build/testTeXMathML/testTeXMathML
+failures=0
 for t in *.tex; do
     $TESTPROG <$t >tmp
     diff ${t%.tex}.xhtml tmp >tmpdiff
     if [ "$?" -ne "0" ]; then
         echo "Test ${t%.tex} FAILED (< expected, > actual):"
         cat tmpdiff
+        let "failures=$failures+1"
     else
         echo "Test ${t%.tex} PASSED"
     fi
 done
+exit $failures
