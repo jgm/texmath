fb516f296e2f16f29d9ce3978a4e29a67c468bbf
John MacFarlane <jgm@berkeley.edu>
2010-10-25 18:43:59 -0700

    Modified cgi to allow macros parameter.

--- cgi/texmath-cgi.hs
+++ cgi/texmath-cgi.hs
@@ -2,19 +2,29 @@ module Main where
 import Network.CGI
 import Text.XML.Light
 import Text.TeXMath
+import Text.TeXMath.Macros
 import Data.Maybe (fromMaybe)
 import Control.Monad
 import Text.JSON
 import Codec.Binary.UTF8.String (decodeString)
+import Text.ParserCombinators.Parsec (parse, many, try)
 
 cgiMain :: CGI CGIResult
 cgiMain = do
-  latexFormula <- liftM (decodeString . fromMaybe "") $ getInput "latexFormula"
   setHeader "Content-type" "text/xhtml; charset=UTF-8"
+  latexFormula <- liftM (decodeString . fromMaybe "") $ getInput "latexFormula"
+  rawMacros <- liftM (decodeString . fromMaybe "") $ getInput "macros"
+  let pMacros = many (try $ pSkipSpaceComments >> pMacroDefinition)
   output . encodeStrict $
-    case texMathToMathML DisplayBlock latexFormula of
-       Left e     -> toJSObject [("success", JSBool False), ("error", JSString $ toJSString e)]
-       Right v    -> toJSObject [("success", JSBool True), ("mathml", JSString $ toJSString $ ppElement v)]
+    case parse pMacros "input" rawMacros of
+         Left err  -> toJSObject [("success", JSBool False)
+                                 ,("error", JSString $ toJSString (show err))]
+         Right ms  -> case texMathToMathML DisplayBlock (applyMacros ms latexFormula) of
+                       Left e  -> toJSObject [("success", JSBool False)
+                                             , ("error", JSString $ toJSString e)]
+                       Right v -> toJSObject [("success", JSBool True)
+                                             , ("mathml", JSString $ toJSString $
+                                                          ppElement v)]
 
 main :: IO ()
 main = runCGI $ handleErrors cgiMain
