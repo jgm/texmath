b9c0126b8fa9eea19c117c5248ebdc7c9b9a49b2
John MacFarlane <jgm@berkeley.edu>
2014-07-22 08:59:34 -0700

    Deleted old Setup.hs.
    This caused the wrong test suite to be run!

--- Setup.hs
+++
@@ -1,7 +0,0 @@
-import Distribution.Simple
-import System.Process (system)
-import System.Exit
-
-main = defaultMainWithHooks $ simpleUserHooks { runTests  = runTestSuite }
-
-runTestSuite _ _ _ _ = system "cd tests && sh runtests.sh" >>= exitWith
