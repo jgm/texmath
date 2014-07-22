74d0906ded35bc0b9b1050165a4e9dea92c02392
Matthew Pickering <matthewtpickering@gmail.com>
2014-07-22 12:07:05 +0100

    Removed XtoY functions from TeXMath

--- src/Text/TeXMath.hs
+++ src/Text/TeXMath.hs
@@ -19,68 +19,22 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 {- | Functions for converting LaTeX math formulas to MathML.
 -}
 
-module Text.TeXMath ( texMathToMathML,
-                      texMathToOMML,
-                      texMathToPandoc,
-                      mathMLToOMML,
-                      mathMLToPandoc,
-                      mathMLToLaTeX,
+module Text.TeXMath (
                       readMathML,
                       readTeXMath,
-                      toTeXMath,
-                      toOMML,
-                      toMathML,
+                      writeTeXMath,
+                      writeTeXMathIn, 
+                      writeOMML,
+                      writeMathML,
+                      writePandoc, 
                       DisplayType(..),
+                      Exp
                       )
 where
-import Text.TeXMath.Parser
-import Text.TeXMath.MathMLParser
-import Text.TeXMath.MathML
-import Text.TeXMath.OMML
-import Text.TeXMath.Pandoc
-import Text.TeXMath.LaTeX
+import Text.TeXMath.Readers.TeXMath
+import Text.TeXMath.Readers.MathML
+import Text.TeXMath.Writers.MathML
+import Text.TeXMath.Writers.OMML
+import Text.TeXMath.Writers.Pandoc
+import Text.TeXMath.Writers.TeXMath
 import Text.TeXMath.Types
-import Text.XML.Light
-import Text.Pandoc.Definition
-import Control.Applicative ((<$>))
-import Data.Maybe (fromMaybe)
-
--- | Convert texMath string to MathML
-texMathToMathML :: DisplayType -> String -> Either String Element
-texMathToMathML dt inp = inp `seq`
-  toMathML dt <$> readTeXMath inp
-
--- | Convert texMath string to OMML (Office Math Markup Language)
-texMathToOMML :: DisplayType -> String -> Either String Element
-texMathToOMML dt inp = inp `seq`
-  toOMML dt <$> readTeXMath inp
-
--- | Convert texMath to Pandoc inlines
-texMathToPandoc :: DisplayType -> String -> Either String [Inline]
-texMathToPandoc dt inp = inp `seq`
-  fromMaybe fallback . toPandoc dt <$> readTeXMath inp
-  where fallback = [Str $ delim ++ inp ++ delim]
-        delim    = case dt of { DisplayInline -> "$"; DisplayBlock -> "$$" }
-
--- | Convert MathML to OMML (Office Math Markup Language)
-mathMLToOMML :: DisplayType -> String -> Either String Element
-mathMLToOMML dt inp = inp `seq`
-   (toOMML dt) <$> readMathML inp
-
--- | Convert MathML to Pandoc inlines
-mathMLToPandoc :: DisplayType -> String -> Either String [Inline]
-mathMLToPandoc dt inp = inp `seq`
-  fromMaybe fallback . toPandoc dt <$> readMathML inp
-  where fallback = [Str $ delim ++ inp ++ delim]
-        delim    = case dt of { DisplayInline -> "$"; DisplayBlock -> "$$" }
-
--- | Convert MathML to Pandoc Math Element
-mathMLToLaTeX :: DisplayType -> String -> Either String Inline
-mathMLToLaTeX dt inp = inp `seq`
-  rt =<< (toTeXMath dt <$> readMathML inp)
-  where
-    mathType = case dt of { DisplayInline -> InlineMath;
-                            DisplayBlock -> DisplayMath }
-    rt s = case s of
-            "" -> Left "Conversion resulted in empty string"
-            _  -> Right $ Math mathType s
