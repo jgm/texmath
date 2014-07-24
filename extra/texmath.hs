c0bf50d72ac2a558a77052b1c7b519761117a0de
John MacFarlane <fiddlosopher@gmail.com>
2014-07-24 12:20:17 -0700

    texmath: Re-removed environment delimiters.

--- extra/texmath.hs
+++ extra/texmath.hs
@@ -54,7 +54,7 @@ readNative s =
 writers :: [(String, Writer)]
 writers = [
     ("native", StringWriter (\_ es -> show es) )
-  , ("tex", StringWriter ((\d -> addLaTeXEnvironment d . writeTeXMath)))
+  , ("tex", StringWriter (\_ -> writeTeXMath))
   , ("omml",  XMLWriter writeOMML)
   , ("xhtml",   XMLWriter (\dt e -> inHtml (writeMathML dt e)))
   , ("mathml",   XMLWriter writeMathML)
