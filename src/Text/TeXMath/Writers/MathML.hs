df1e5e74d7625bafc2ba2092dd7a6e7efae2fce7
John MacFarlane <jgm@berkeley.edu>
2026-01-17 13:17:11 +0100

    MathML writer: ensure that bolded mi stay italic.
    We do this by making the mathvariant `bold-italic` rather than `bold`
    when applying boldface to an `mi` element.
    
    Closes #280.

--- src/Text/TeXMath/Writers/MathML.hs
+++ src/Text/TeXMath/Writers/MathML.hs
@@ -133,7 +133,10 @@ makeArray tt as ls = unode "mtable" $
 
 -- Kept as String for Text.XML.Light
 withAttribute :: String -> T.Text -> Element -> Element
-withAttribute a = add_attr . Attr (unqual a) . T.unpack
+withAttribute "mathvariant" "bold" e
+   | elName e == unqual "mi" -- special case see #280
+   = add_attr (Attr (unqual "mathvariant") "bold-italic") e
+withAttribute a b e = add_attr (Attr (unqual a) (T.unpack b)) e
 
 -- Preserves the order of any existing attributes
 setAttribute :: String -> T.Text -> Element -> Element
